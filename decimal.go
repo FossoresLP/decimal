d810b8431effabbd5762666e575e482d86ea5970
Pascal Vorwerk <info@fossores.de>
2024-08-24 14:08:49 +0200

    feat: improve precision handling in FromFloat64 and introduce FromFloat64Fixed
    Refactor the `FromFloat64` and `FromFloat64Fixed` methods in the `Decimal` struct to improve precision handling. This enhancement addresses precision limitations when iteratively dividing floats and improves the accuracy of decimal conversions. It also improves performance considerably.
    `FromFloat64Fixed` has been introduced to generate output with a fixed precision and is extremely fast and consistent.

--- decimal.go
+++ decimal.go
@@ -3,7 +3,6 @@ package decimal
 import (
 	"database/sql/driver"
 	"fmt"
-	"math"
 
 	"github.com/fxamacker/cbor/v2"
 )
@@ -215,26 +214,31 @@ fracloop:
 	return nil
 }
 
+// FromFloat64 converts a float64 to a decimal with up to 18 digits after the decimal point, avoiding trailing zeros.
+// Digits beyond the 18th digit after the decimal point are truncated and values above MaxInt64 and below MinInt64 can't be converted. NaN and Inf are not supported.
 func (d *Decimal) FromFloat64(f float64) {
-	if f < 0 {
-		d.Negative = true
+	d.FromFloat64Fixed(f, 18)
+	if d.Fraction == 0 {
+		d.Digits = 0
+		return
+	}
+	for d.Fraction%10 == 0 {
+		d.Fraction /= 10
+		d.Digits--
+	}
+}
+
+// FromFloat64Fixed converts a float64 to a decimal with a fixed precision, truncating any additional digits and potentially leading to trailing zeros.
+func (d *Decimal) FromFloat64Fixed(f float64, digits uint8) {
+	if digits > 18 {
+		digits = 18
+	}
+	d.Negative = f < 0
+	if d.Negative {
 		f = -f
 	}
 	d.Integer = uint64(f)
-	fraction := math.Abs(f - float64(d.Integer))
-
-	const maxDigits = 18 // Reasonable precision to handle float64 precision limits
-	var digits uint8
-
-	for i := 0; i < maxDigits; i++ {
-		fraction *= 10
-		digits++
-		if fraction == math.Round(fraction) {
-			break
-		}
-	}
-
-	d.Fraction = uint64(fraction)
+	d.Fraction = uint64((f - float64(d.Integer)) * float64(pow10[digits]))
 	d.Digits = digits
 }
 
